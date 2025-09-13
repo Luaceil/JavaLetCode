1afd8b0e2fdc5cdfefd755f898b57626c085514f
daiqiu <3322703718@qq.com>
2025-09-14 02:55:46 +0800

    hit oppo

--- OPP/src/yu/Main.java
+++ OPP/src/yu/Main.java
@@ -1,4 +1,5 @@
 package yu;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
@@ -23,8 +24,17 @@ public class Main {
             for (int i = 0; i < N; i++) {
                 brr[i] = in.nextInt();
             }
+            System.out.println();
+            System.out.print("arr:");
+            Arrays.stream(arr).forEach(a->{System.out.print(a+" ");});
+            System.out.println();
+            System.out.print("brr:");
+            Arrays.stream(brr).forEach(b->{System.out.print(b+" ");});
             boolean fl=true;
+            System.out.println();
             for (int i = 0; i < N; i++) {
+                System.out.println();
+                System.out.print(i+":");
                 Set<Integer> allset = new HashSet<>();
                 //记录需要满足的set
                 for (int j = 0; j < 31; j++) {
@@ -32,13 +42,16 @@ public class Main {
                         allset.add(j);
                     }
                 }
+                System.out.println();
+                System.out.print("allset:");
+                allset.forEach(b->{System.out.print(b+" ");});
                 Set<Integer> set = new HashSet<>(allset);
-                for (int j = 0; j < N; j++) {
+                for (int j = 0; j <=i; j++) {
                     boolean[] ll = new boolean[31];
                     boolean flag = true;
                     for (int k = 0; k < 31; k++) {
                         if (((arr[j] >> k) & 1) == 1) {
-                            if (!allset.contains(j)) { //超界 舍弃
+                            if (!allset.contains(k)) { //超界 舍弃
                                 flag =false;
                                 break;
                             }
@@ -46,16 +59,21 @@ public class Main {
                         }
                     }
                     if(!flag) continue;
+                    System.out.print("hit:");
                     for(int k=0;k<31;k++){
                         if(ll[k]){
-                            if (set.contains(k)) set.remove(k);
+                                    System.out.print(k+" ");
+                            if (set.contains(k)) {set.remove(k);}
                         }
                     }
                 }
-                if(!set.isEmpty()){fl=false;}
-                if(set.isEmpty()){System.out.println("Yes");}else System.out.println("No");
+                System.out.print("restSet:");
+                set.forEach(b->{System.out.print(b+" ");});
+                if(!set.isEmpty()){fl=false;break;}
+                System.out.println();
             }
-           // if(fl){System.out.println("Yes");}else System.out.println("No");
+            System.out.println();
+            if(fl){System.out.println("Yes");}else System.out.println("No");
         }
 
     }
